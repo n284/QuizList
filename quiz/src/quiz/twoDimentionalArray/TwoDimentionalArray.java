d648d308836d98112f5e51837ec69225a56cd1d8
souken <souken@ginga316>
2022-10-14 07:14:14 +0900

    class upload.

---
+++ quiz/src/quiz/twoDimentionalArray/TwoDimentionalArray.java
@@ -0,0 +1,25 @@
+package quiz.twoDimentionalArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TwoDimentionalArray {
+
+	public static int ROW_NUMBER = 3;
+	public static int COLUMN_NUMBER = 3;
+
+	public static void main(String[] args) {
+		int [][] oneArray = new int[ROW_NUMBER][COLUMN_NUMBER];
+		int [][] twoArray = new int [ROW_NUMBER][COLUMN_NUMBER];
+
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			for(int i = 0; i < ROW_NUMBER; i++) {
+
+			}
+		}catch (IOException e) {
+
+		}
+	}
+
+}
