3772c4ec13fd1494909be43446b4b8a1ea6b6f01
nishizaw-a <n00284k@gmail.com>
2022-11-11 00:10:31 +0900

    Upload files.

---
+++ quiz/src/quiz/asciiArt1/AsciiArt1.java
@@ -0,0 +1,9 @@
+package quiz.asciiArt1;
+
+public class AsciiArt1 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ quiz/src/quiz/asciiArt1/AsciiArt1Answer.java
@@ -0,0 +1,9 @@
+package quiz.asciiArt1;
+
+public class AsciiArt1Answer {
+
+	public static void main(String[] args) {
+
+	}
+
+}
